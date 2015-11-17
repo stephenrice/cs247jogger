d27f04ae63c9befeef289ae7a7feacd07dbdeec2
Stephen Rice <srice120@gmail.com>
2015-11-16 16:34:26 -0800

    Initial Commit

---
+++ Jogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jogger
+//
+//  Created by Stephen Rice on 11/16/15.
+//  Copyright (c) 2015 Stephen Rice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
